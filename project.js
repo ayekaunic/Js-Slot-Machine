4d17fe8361790f119b15e467275961446a623295
ayekaunic <ayekaunic@gmail.com>
2023-06-22 16:21:23 +0500

    collect bet.

--- project.js
+++ project.js
@@ -14,7 +14,7 @@ const deposit = () => {
 
 const getNumberOfLines = () => {
     while (true) {
-        const lines = prompt('Enter the number of lines you want to bet on: ');
+        const lines = prompt('Enter the number of lines to bet on (1-3): ');
         const numberOfLines = parseFloat(lines);
         if (isNaN(numberOfLines) || numberOfLines < 1 || numberOfLines > 3) {
             console.log('Invalid number of lines, try again.');
@@ -24,5 +24,18 @@ const getNumberOfLines = () => {
     }
 }
 
-const depositAmount = deposit();
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt('Enter the bet per line: ');
+        const numberBet = parseFloat(bet);
+        if (isNaN(numberBet) || numberBet < 1 || numberBet > balance / lines) {
+            console.log('Inalid bet, try again.');
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const betAmount = getBet(balance, numberOfLines);
